b84e2b59b093302245918d78abe6998ec7c534bd
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-21 15:39:21 -0400

    create handleClick

--- src/index.js
+++ src/index.js
@@ -24,6 +24,12 @@ class Board extends React.Component {
     }
   }
 
+  handleCkick(i) {
+    const squares = this.state.squares.slice();
+    squares[i]= 'X'
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
     return (
       <Square 
