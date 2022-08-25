9aa9d454d38c2105628a00ee06ec9c35e9e7e6eb
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-25 10:52:25 -0400

    set state and allow players to take turns

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function Square (props) {
     return (
       <button 
         className="square" 
-        onClick={() => props.onClick()}>
+        onClick={props.onClick}>
           {props.value}
       </button>
     );
@@ -18,14 +18,18 @@ class Board extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleCkick(i) {
     const squares = this.state.squares.slice();
-    squares[i]= 'X'
-    this.setState({squares: squares})
+    squares[i]= this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 
   renderSquare(i) {
